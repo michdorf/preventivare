3c5d08ea0acec15becb6cff0b2963484e4b26c95
michdorf <michele@dechiffre.dk>
2022-05-12 06:58:32 +0000

    Agg. spesa interfaccia

---
+++ src/interfacce/spesa.ts
@@ -0,0 +1,15 @@
+import type Ammonta from "./ammonta";
+import type Categoria from "./categoria";
+import type Ricorrente from '../../moduli/moduli/ricorrente';
+
+export default interface Spesa {
+    id: string;
+    categoria: Categoria;
+    etichette: string[];
+    commento: string;
+    ammonta: Ammonta;
+    pagato: boolean;
+    data: Date; /** Data che devi usare i soldi */
+    ripeti: Ricorrente;
+    creato: Date;
+}
