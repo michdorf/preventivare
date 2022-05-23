2c6d327877aad23e33d855fce6eb7dec6f571c49
Michele Dorph De Chiffre <md@autonik.dk>
2022-05-23 14:25:31 +0200

    Fa scegliere una categoria parente

--- src/comps/categorie/agg-categoria.svelte
+++ src/comps/categorie/agg-categoria.svelte
@@ -2,30 +2,28 @@
 import uuid from "../../../moduli/memo/uuid";
 import type Ammonta from "../../interfacce/ammonta";
 
-    import type Categoria from "../../interfacce/categoria";
-    import categorie from "../../servizio/categorie";
+import type Categoria from "../../interfacce/categoria";
+import categorie from "../../servizio/categorie";
 import ValutaSelect from "../valuta-select.svelte";
+import CategoriaSelect from './select.svelte';
 
-    let c: Categoria = {
-        id: uuid(),
-        titolo: 'Test',
-        parente: null,
-        stima: '22:eur',
-        creato: Date.now()
-    };
 
-    let stima = 0;
-    $: {
-        c.stima = (stima + ':' + 'dkk' as Ammonta);
-    }
+let c: Categoria = {
+    id: uuid(),
+    titolo: 'Test',
+    parente: null,
+    stima: '22:eur',
+    creato: Date.now()
+};
+
+let stima = 0;
+$: {
+    c.stima = (stima + ':' + 'dkk' as Ammonta);
+}
 </script>
 
 <input bind:value={c.titolo} placeholder="Titel" /> <br>
-<select>
-    <optgroup label="Vælg en overkategori">
-        <option value="" selected>Ingen</option>
-    </optgroup>
-</select> <br>
+<CategoriaSelect bind:value={c.parente}></CategoriaSelect> <br>
 
 <input bind:value={stima} placeholder="Estimat" /> <ValutaSelect></ValutaSelect><br>
 <button on:click={() => {categorie.agg(c)}}>Click</button>
---
+++ src/comps/categorie/select.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+import { derived, Writable } from "svelte/store";
+import type Categoria from "../../interfacce/categoria";
+import categorie from "../../servizio/categorie";
+
+export let value: string = "";
+export let curCategoria: string = "";
+    
+let c = derived(categorie.data as Writable<Categoria[]>, value => {
+    return value.filter((v) => v.id !== curCategoria);
+});
+</script>
+
+<select bind:value={value}>
+    <optgroup label="Vælg en overkategori">
+        {#each $c as categoria}
+            <option value={categoria.id}>{categoria.titolo}</option>
+        {/each}
+        <option value="" selected>Ingen</option>
+    </optgroup>
+</select>
