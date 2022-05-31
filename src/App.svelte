99fd4b448885de46328563a3dcb8a09a689ae72d
Michele Dorph de Chiffre <michele@dorph.dk>
2022-05-31 11:10:38 +0200

    Implementa slide up carta

--- src/App.svelte
+++ src/App.svelte
@@ -2,28 +2,51 @@
 import { get } from 'svelte/store'
 import ListaCategorie from "./comps/categorie/lista-categorie.svelte";
 import ListaSpese from './comps/spese/lista-spese.svelte';
+import SlideUpCarta from './comps/slide-up-carta.svelte';
 import AggCategoria from "./comps/categorie/agg-categoria.svelte";
 import AggSpesa from "./comps/spese/agg-spesa.svelte";
 import categorie from "./servizio/categorie";
 
+let showSlideUpCarta = false;
+let slideUpCartaProps = {};
+let SlideUpCartaComp = null;
+
 export let name: string;
 let curCatId = "";
 $: curCategoria = get(categorie.data).find(c => c.id === curCatId);
+
+function mostraAggSpesa() {
+	SlideUpCartaComp = AggSpesa;
+	slideUpCartaProps = {categoria: curCategoria};
+	showSlideUpCarta = true;
+}
+$: {
+	if (showSlideUpCarta) {
+		window.scrollTo(0,0);
+	}
+}
 </script>
 
-<main>
+<main style={(showSlideUpCarta ? "transform:scale(0.98);" : "")}>
 	<h1>Hello {name}!</h1>
 	<ListaCategorie on:select={catId => {curCatId = catId.detail}}></ListaCategorie>
 	<hr>
 	<AggCategoria></AggCategoria>
 	<hr>
 	{#if curCategoria}
-		<AggSpesa categoria={curCategoria}></AggSpesa>
+		<button on:click={mostraAggSpesa}>Aggiungi Spesa</button>
+		<!-- <AggSpesa categoria={curCategoria}></AggSpesa> -->
 	{/if}
 	<hr>
 	<ListaSpese></ListaSpese>
 	<hr>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+
+	{#if showSlideUpCarta}
+	<SlideUpCarta on:nascondi={() => showSlideUpCarta = false}>
+		<svelte:component this={SlideUpCartaComp} {...slideUpCartaProps}></svelte:component>
+	</SlideUpCarta>
+	{/if}
 </main>
 
 <style>
---
+++ src/comps/slide-up-carta.svelte
@@ -0,0 +1,29 @@
+<div class="pagina" transition:slide={{duration: 200}}>
+    <button on:click={nascondi} style="float: right">X</button>
+    <slot></slot>
+</div>
+
+<script lang="ts">
+    import {slide} from 'svelte/transition';
+    import {createEventDispatcher} from 'svelte';
+
+    const dispatch = createEventDispatcher();
+
+    function nascondi() {
+        dispatch("nascondi");
+    }
+</script>
+
+<style>
+    .pagina {
+        box-sizing: border-box;
+        padding: 0.6em;
+        box-shadow: 0 -0.1em 0.4em 0 rgba(0,0,0,0.3);
+        background-color: var(--bg-color);
+        height: 95vh;
+        width: 100%;
+        position: fixed;
+        bottom: 0; 
+        border-radius: var(--border-radius) var(--border-radius) 0 0
+    }
+</style>
